6329fb449484fe257cd2b57202ff7143c80ecbf2
ThriftyOldStudent <asvoria@live.com>
2022-08-01 00:09:59 +0800

    e

--- src/index.js
+++ src/index.js
@@ -9,19 +9,20 @@ const SHOMIN_ABI = [{
     'outputs': [],
     'stateMutability': 'payable',
     'type': 'function'
-}, {
-  'inputs': [],
-  'name': 'getLastID',
-  'outputs': [{ 'internalType': 'uint256', 'name': '', 'type': 'uint256' }],
-  'stateMutability': 'view',
-  'type': 'function',
-}, {
-  'inputs': [{ 'internalType': 'uint256', 'name': 'tokenId', 'type': 'uint256' }],
-  'name': 'ownerOf',
-  'outputs': [{ 'internalType': 'address', 'name': '', 'type': 'address' }],
-  'stateMutability': 'view',
-  'type': 'function',
-}]
+  }, {
+    'inputs': [],
+    'name': 'getLastID',
+    'outputs': [{ 'internalType': 'uint256', 'name': '', 'type': 'uint256' }],
+    'stateMutability': 'view',
+    'type': 'function',
+  }, {
+    'inputs': [{ 'internalType': 'uint256', 'name': 'tokenId', 'type': 'uint256' }],
+    'name': 'ownerOf',
+    'outputs': [{ 'internalType': 'address', 'name': '', 'type': 'address' }],
+    'stateMutability': 'view',
+    'type': 'function',
+  },
+]
 
 const submitOrder = document.getElementById('order')
 const Web3 = require('web3')
