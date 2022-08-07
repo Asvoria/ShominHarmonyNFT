8af26180cb1876f40953c44237145803a4cc8998
ThriftyOldStudent <asvoria@live.com>
2022-08-07 12:36:09 +0800

    r

--- src/index.js
+++ src/index.js
@@ -166,19 +166,15 @@ const runMetamask = () => {
       const passVar = `${_accounts[0]}`
       if (ChainID === HarmonyChainID) {
         let OwnerCheckONE = 0
-        const BalanceInContractONE = await SHOMINcontractONE.methods.balanceOf(passVar).encodeABI().call().then((balance) => {
-          console.log(balance)
-          if (BalanceInContractONE > 0) {
-            OwnerCheckONE = BalanceInContractONE[0]
-          }
-        })
+        const BalanceInContractONE = await SHOMINcontractONE.methods.balanceOf(passVar).encodeABI().call((err, result) => { console.log(result) })
+        if (BalanceInContractONE > 0) {
+          OwnerCheckONE = BalanceInContractONE[0]
+        }
 
         ContentArea.innerHTML = `<div id="sscONE">ONE: ${OwnerCheckONE}</div>`
       } else if (ChainID === BinanceChainID) {
         let OwnerCheckBNB = 0
-        const BalanceInContractBNB = await SHOMINcontractBNB.methods.balanceOf(passVar).encodeABI().call().then((balance) => {
-          console.log(balance)
-        })
+        const BalanceInContractBNB = await SHOMINcontractBNB.methods.balanceOf(passVar).encodeABI().call((err, result) => { console.log(result) })
         if (BalanceInContractBNB > 0) {
           OwnerCheckBNB = BalanceInContractBNB[0]
         }
