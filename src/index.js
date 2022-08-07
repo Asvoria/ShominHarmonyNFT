123e6109b4356bc54c42df9f2ff9a6bf813cd0c9
ThriftyOldStudent <asvoria@live.com>
2022-08-07 10:19:36 +0800

    r

--- src/index.js
+++ src/index.js
@@ -27,11 +27,11 @@ const SHOMIN_ABI = [{
   'stateMutability': 'view',
   'type': 'function',
 }, {
-  "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }],
-  "name": "balanceOf",
-  "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-  "stateMutability": "view",
-  "type": "function",
+  'inputs': [{ 'internalType': 'address', 'name': 'owner', 'type': 'address' }],
+  'name': 'balanceOf',
+  'outputs': [{ 'internalType': 'uint256', 'name': '', 'type': 'uint256' }],
+  'stateMutability': 'view',
+  'type': 'function',
 }]
 
 const TitleText = document.getElementById('Title')
