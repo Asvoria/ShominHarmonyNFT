540e1f2b28c4a7dc600f671e7f52cd20c1acab90
ThriftyOldStudent <asvoria@live.com>
2022-08-11 00:09:03 +0800

    s

--- src/index.js
+++ src/index.js
@@ -75,8 +75,8 @@ const web3ONE = new Web3('https://api.s0.ps.hmny.io')
 const web3BNB = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545/')
 const HarmonyChainID = '1666900000'
 const BinanceChainID = '97'
-const contractAddsONE = '0xa7c419E435D628Aa159c843D932F8E87fCa4e633'
-const contractAddsBNB = '0xbE8ea518fA40f722395dC91e691f5f17C91f2b38'
+const contractAddsONE = '0xa6f1b130C69A4349a9Fc51bB36e01Ab2cD925EFB'
+const contractAddsBNB = '0x7044C20c5094697048791d4dDFf7CE56724e03Ff'
 const SHOMINcontractONE = new web3ONE.eth.Contract(SHOMIN_ABI, contractAddsONE)
 const SHOMINcontractBNB = new web3BNB.eth.Contract(SHOMIN_ABI, contractAddsBNB)
 
