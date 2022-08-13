032a93a17002153fd7169f00a05ce5ed6f395465
ThriftyOldStudent <asvoria@live.com>
2022-08-13 09:07:29 +0800

    big

--- README.md
+++ README.md
@@ -10,15 +10,24 @@ npx prettier --write
 
 const web3ONE = new Web3('https://api.harmony.one')
 const web3BNB = new Web3('https://bsc-dataseed.binance.org/')
+const web3CELO = new Web3('https://alfajores-forno.celo-testnet.org')
 const HarmonyChainID = '1666600000'
 const BinanceChainID = '56'
+const CeloChainID = '44787'
+const contractAddsONE = '0xAa993f353aA3Dc670237e73b08533D0adA45Db5A'
+const contractAddsBNB = '0x54e0e3b3adC1FD0FAbCa8e97fd43CBE24eF2E206'
+const contractAddsCELO = '0x2E5adC0D61bd932C215bc7850b80Ff1602121db7'
+
 
 const web3ONE = new Web3('https://api.s0.ps.hmny.io')
 const web3BNB = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545/')
+const web3CELO = new Web3('https://forno.celo.org')
 const HarmonyChainID = '1666900000'
 const BinanceChainID = '97'
+const CeloChainID = '42220'
 const contractAddsONE = '0xa6f1b130C69A4349a9Fc51bB36e01Ab2cD925EFB'
 const contractAddsBNB = '0x7044C20c5094697048791d4dDFf7CE56724e03Ff'
+const contractAddsCELO = '0xf520dA05364af929e19866Bbd6c4fFAC2eca0EDa'
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import MetaMaskOnboarding from '@metamask/onboarding'
 
-const SHOMIN_ABI = [{
+const SSC_ABI = [{
   'inputs': [{ 'internalType': 'address', 'name': 'owner', 'type': 'address' }],
   'name': 'balanceOf',
   'outputs': [{ 'internalType': 'uint256', 'name': '', 'type': 'uint256' }],
@@ -30,65 +30,50 @@ const SHOMIN_ABI = [{
   'outputs': [{ 'internalType': 'string', 'name': '', 'type': 'string' }],
   'stateMutability': 'view',
   'type': 'function',
-}, {
-  'anonymous': false,
-  'inputs': [
-    { 'indexed': true, 'internalType': 'address', 'name': 'owner', 'type': 'address' },
-    { 'indexed': true, 'internalType': 'address', 'name': 'approved', 'type': 'address' },
-    { 'indexed': true, 'internalType': 'uint256', 'name': 'tokenId', 'type': 'uint256' },
-  ],
-  'name': 'Approval',
-  'type': 'event',
-}, {
-  'anonymous': false,
-  'inputs': [
-    { 'indexed': true, 'internalType': 'address', 'name': 'owner', 'type': 'address' },
-    { 'indexed': true, 'internalType': 'address', 'name': 'operator', 'type': 'address' },
-    { 'indexed': false, 'internalType': 'bool', 'name': 'approved', 'type': 'bool' },
-  ],
-  'name': 'ApprovalForAll',
-  'type': 'event',
-}, {
-  'anonymous': false,
-  'inputs': [
-    { 'indexed': true, 'internalType': 'address', 'name': 'from', 'type': 'address' },
-    { 'indexed': true, 'internalType': 'address', 'name': 'to', 'type': 'address' },
-    { 'indexed': true, 'internalType': 'uint256', 'name': 'tokenId', 'type': 'uint256' },
-  ],
-  'name': 'Transfer',
-  'type': 'event',
 }]
 
 const sleep = (ms) => new Promise((res) => setTimeout(res, ms))
 
 const TitleText = document.getElementById('Title')
-const BUTTON = document.getElementById('BUTTON')
-const BUTTONb = document.getElementById('BUTTONb')
-const buyBUTTONbONE = document.getElementById('buyBUTTONbONE')
-const buyBUTTONbBNB = document.getElementById('buyBUTTONbBNB')
-const AREAm = document.getElementById('MessageArea')
-const ContentArea = document.getElementById('ContentArea')
+const BTNlogin = document.getElementById('BTNlogin')
+const buyBTN = document.getElementById('buyBTN')
+const buyBTNtext = document.getElementById('buyBTNtext')
+const MsgArea = document.getElementById('MsgArea')
+const CntArea = document.getElementById('CntArea')
 
 const Web3 = require('web3')
 
 const web3ONE = new Web3('https://api.harmony.one')
 const web3BNB = new Web3('https://bsc-dataseed.binance.org/')
-const HarmonyChainID = '1666600000'
-const BinanceChainID = '56'
-const contractAddsONE = '0xAa993f353aA3Dc670237e73b08533D0adA45Db5A'
-const contractAddsBNB = '0x54e0e3b3adC1FD0FAbCa8e97fd43CBE24eF2E206'
-const SHOMINcontractONE = new web3ONE.eth.Contract(SHOMIN_ABI, contractAddsONE)
-const SHOMINcontractBNB = new web3BNB.eth.Contract(SHOMIN_ABI, contractAddsBNB)
+const web3CELO = new Web3('https://forno.celo.org')
+const ChainWeb3 = ['0', web3ONE, web3BNB, web3CELO]
+
+const ChainConAdr = [
+  '0',
+  '0xAa993f353aA3Dc670237e73b08533D0adA45Db5A',
+  '0x54e0e3b3adC1FD0FAbCa8e97fd43CBE24eF2E206',
+  '0xf520dA05364af929e19866Bbd6c4fFAC2eca0EDa',
+]
+const conAbiONE = new web3ONE.eth.Contract(SHOMIN_ABI, ChainConAdr[0])
+const conAbiBNB = new web3BNB.eth.Contract(SHOMIN_ABI, ChainConAdr[1])
+const conAbiCELO = new web3CELO.eth.Contract(SHOMIN_ABI, ChainConAdr[2])
+const ChainConABI = ['0', conAbiONE, conAbiBNB, conAbiCELO]
+
+const ChainName = ['ERROR', 'Harmony Shard 0 Mainnet', 'Binance Smart Chain', 'Celo Mainnet']
+const ChainShort = ['ERROR', 'HARMONY', 'BINANCE', "CELO"]
+const ChainSym = ['ERROR', 'ONE', 'BNB', "CELO"]
+const ChainID = ['0', '1666600000', '56', '42220']
+const ChainPrice = [Number(0), Number(200.0), Number(0.0068), Number(2)]
+const ChainStrURL = [
+  'ERROR', 
+  'https://asvoria.github.io/Author/nft/SCCcard2022v1.json',
+  'https://asvoria.github.io/Author/nft/SCCcard2022v2.json',
+  'https://asvoria.github.io/Author/nft/SCCcard2022v2.json',
+]
 
 const currentUrl = new URL(window.location.href)
 const forwarderOrigin =
   currentUrl.hostname === 'localhost' ? 'http://localhost:9010' : undefined
-
-const totalPriceONE = Number(200.0)
-const totalPriceBNB = Number(0.0068)
-
-const strURLONE = 'https://asvoria.github.io/Author/nft/SCCcard2022v1.json'
-const strURLBNB = 'https://asvoria.github.io/Author/nft/SCCcard2022v2.json'
 
 const runMetamask = () => {
   const isMetaMaskInstalled = () => {
@@ -97,43 +82,42 @@ const runMetamask = () => {
   }
   const onboarding = new MetaMaskOnboarding({ forwarderOrigin })
 
-  const onClickBuyONE = async () => {
+  const onClickBuyX = async (cName, cID, cWeb3, cConABI, cPrice, cURL, cConAds) => {
     try {
       await ethereum.request({ method: 'eth_requestAccounts' })
       const ChainID = await ethereum.request({ method: 'net_version' })
       let txlog = ''
 
-      if (ChainID === HarmonyChainID) {
+      if (ChainID === cID) {
         const _accounts = await ethereum.request({
           method: 'eth_accounts',
         })
 
-        const totalONE = await totalPriceONE * (10 ** 18)
-        const txHash = await SHOMINcontractONE.methods.buyMembership(strURLONE).encodeABI()
+        const totalToken = await cPrice * (10 ** 18)
+        const txHash = await cConABI.methods.buyMembership(cURL).encodeABI()
         const txO = await ethereum.request({
           method: 'eth_sendTransaction',
           params: [{
-            to: contractAddsONE,
+            to: cConAds,
             from: _accounts[0],
-            value: web3ONE.utils.toHex(totalONE),
+            value: cWeb3.utils.toHex(totalToken),
             data: txHash,
           }],
         }).then((result) => {
-          console.log('ONE chain response: ')
+          console.log('Chain response: ')
           console.log(result)
           txlog = result
         }).catch((error) => {
           console.log(error)
         })
         console.log(txO)
-        console.log(txlog)
         await sleep(600)
         let getRxLog = ''
         for (let i = 0; i < 30; i++) {
-          console.log('waiting...')
-          getRxLog = await web3ONE.eth.getTransactionReceipt(txlog)
-          AREAm.innerText += `.`
-          await sleep(100)
+          console.log('Waiting...')
+          getRxLog = await cWeb3.eth.getTransactionReceipt(txlog)
+          MsgArea.innerText += `.`
+          await sleep(200)
           if (getRxLog) {
             console.log('rx not null...')
             break
@@ -142,180 +126,110 @@ const runMetamask = () => {
         }
 
         console.log(getRxLog.logs[0].topics[3])
-        const idOnly = await web3ONE.utils.hexToNumber(getRxLog.logs[0].topics[3])
+        const idOnly = await cWeb3.utils.hexToNumber(getRxLog.logs[0].topics[3])
         console.log(idOnly)
-        buyBUTTONbONE.classList.add('hideclass')
-        buyBUTTONbONE.classList.remove('is-visible')
-        buyBUTTONbBNB.classList.add('hideclass')
-        buyBUTTONbBNB.classList.remove('is-visible')
-        AREAm.classList.remove('Error')
-        AREAm.classList.add('Success')
-        AREAm.innerText = `Thank You for your support!\nYou may add the NFT token to your Metamask Mobile Wallet with following informtion.\nContract Address: ${contractAddsONE}\nToken ID: ${idOnly}\nRefresh the page and connect to Metamask to view the secret contents!`
+        buyBTN.classList.add('hideclass')
+        buyBTN.classList.remove('is-visible')
+        MsgArea.classList.remove('Error')
+        MsgArea.classList.add('Success')
+        CntArea.innerText = `Thank You for your support!\nYou may add the NFT token to your Metamask Mobile Wallet with following informtion.\nContract Address: ${cConAds}\nToken ID: ${idOnly}\nRefresh the page and connect to Metamask to view the secret contents!`
       } else {
-        AREAm.classList.add('Error')
-        AREAm.innerText = `Your selected network on Metamask Wallet does not match! Please Select Harmony Shard 0 Mainnet.`
+        MsgArea.classList.add('Error')
+        MsgArea.innerText = `Your selected network on Metamask Wallet does not match! Please Select ${cName}.`
       }
     } catch (error) {
       console.error('error')
       console.error(error)
-      AREAm.innerText = `Network busy. Don't worry, check out your transaction hash and refresh this page.`
+      CntArea.innerText = `Network busy. Don't worry, check out your transaction hash and refresh this page.`
     }
   }
 
-  const onClickBuyBNB = async () => {
+  const CheckOwner = async (chkName, chkShort, chkSym, chkConABI, chkPrice) => {
     try {
-      await ethereum.request({ method: 'eth_requestAccounts' })
-      const ChainID = await ethereum.request({ method: 'net_version' })
-      let txlog = ''
-
-      if (ChainID === BinanceChainID) {
-        const _accounts = await ethereum.request({
-          method: 'eth_accounts',
-        })
-
-        const totalBNB = await totalPriceBNB * (10 ** 18)
-        const txHash = await SHOMINcontractBNB.methods.buyMembership(strURLBNB).encodeABI()
-        const txO = await ethereum.request({
-          method: 'eth_sendTransaction',
-          params: [{
-            to: contractAddsBNB,
-            from: _accounts[0],
-            value: web3BNB.utils.toHex(totalBNB),
-            data: txHash,
-          }],
-        }).then((result) => {
-          console.log('BNB chain response: ')
-          console.log(result)
-          txlog = result
-        }).catch((error) => {
-          console.log(error)
-        })
-        console.log(txO)
-        console.log(txlog)
-        await sleep(600)
-        let getRxLog = ''
-        for (let i = 0; i < 20; i++) {
-          console.log('waiting...')
-          getRxLog = await web3BNB.eth.getTransactionReceipt(txlog)
-          AREAm.innerText += `.`
-          await sleep(100)
-          if (getRxLog) {
-            console.log('rx not null...')
-            break
-          }
-          await sleep(600)
-        }
-
-        console.log(getRxLog.logs[0].topics[3])
-        const idOnly = await web3BNB.utils.hexToNumber(getRxLog.logs[0].topics[3])
-        console.log(idOnly)
-        buyBUTTONbBNB.classList.add('hideclass')
-        buyBUTTONbBNB.classList.remove('is-visible')
-        buyBUTTONbBNB.classList.add('hideclass')
-        buyBUTTONbBNB.classList.remove('is-visible')
-        AREAm.classList.remove('Error')
-        AREAm.classList.add('Success')
-        AREAm.innerText = `Thank You for your support!\nYou may add the NFT token to your Metamask Mobile Wallet with following informtion.\nContract Address: ${contractAddsBNB}\nToken ID: ${idOnly}\nRefresh the page and connect to Metamask to view the secret contents!`
-      } else {
-        AREAm.classList.add('Error')
-        AREAm.innerText = `Your selected network on Metamask Wallet does not match! Please Select Binance Smart Chain Mainnet.`
+      let OwnToken = ''
+      const BalanceInContract = await chkConABI.methods.balanceOf(_accounts[0]).call()
+      if (BalanceInContract > 0) {
+        OwnToken = BalanceInContract
       }
+      CntArea.innerHTML = `<div>Passes on ${chkName}: ${OwnToken}</div>`
+      buyBTNtext.innerHTML = `${chkShort}<br> ${chkPrice} ${chkSym}`
     } catch (error) {
-      console.error('error')
       console.error(error)
-      AREAm.innerText = `Network busy. Don't worry, check out your transaction hash and refresh this page.`
     }
+    return OwnToken
   }
 
   const onClickConnect = async () => {
-    let OwnerCheckONE = 0
-    let OwnerCheckBNB = 0
-    let PassOwner = 0
     try {
       await ethereum.request({ method: 'eth_requestAccounts' })
       const _accounts = await ethereum.request({
         method: 'eth_accounts',
       })
-      const ChainID = await ethereum.request({ method: 'net_version' })
+      const ChainIDsel = await ethereum.request({ method: 'net_version' })
       console.log(_accounts[0])
-      if (ChainID === HarmonyChainID) {
-        const BalanceInContractONE = await SHOMINcontractONE.methods.balanceOf(_accounts[0]).call()
-        if (BalanceInContractONE > 0) {
-          OwnerCheckONE = BalanceInContractONE
-        }
-        ContentArea.innerHTML = `<div id="sscONE">Passes on Harmony: ${OwnerCheckONE}</div>`
-        buyBUTTONbONE.classList.add('is-visible')
-        buyBUTTONbONE.classList.remove('hideclass')
-      } else if (ChainID === BinanceChainID) {
-        const BalanceInContractBNB = await SHOMINcontractBNB.methods.balanceOf(_accounts[0]).call()
-        console.log(BalanceInContractBNB)
-        if (BalanceInContractBNB > 0) {
-          OwnerCheckBNB = BalanceInContractBNB[0]
-        }
-        ContentArea.innerHTML = `<div id="sscBNB">Passes on Binance: ${OwnerCheckBNB}</div>`
-        buyBUTTONbBNB.classList.add('is-visible')
-        buyBUTTONbBNB.classList.remove('hideclass')
+      console.log(ChainID)
+      let ownNFTbalance = 0
+      let ChainSelect = ''
+
+      if (ChainIDsel === ChainID[0]) {
+        ChainSelect = Number(1)
+      } else if (ChainIDsel === ChainID[1]) {
+        ChainSelect = Number(2)
+      } else if (ChainIDsel === ChainID[2]) {
+        ChainSelect = Number(3)
       } else {
+        ChainSelect = Number(0)
         console.log('Wrong Chain!')
-        AREAm.classList.add('Error')
-        AREAm.innerText = `Wrong network detected!\nPlease set Metamask Network to Harmony Shard 0 Mainnet or Binance Smart Chain Mainnet.`
-        TitleText.innerText = 'Please change to the correct Metamask network and then refresh the page.'
       }
 
-      if (OwnerCheckONE > 0) {
-        PassOwner += OwnerCheckONE
-      }
-      if (OwnerCheckBNB > 0) {
-        PassOwner += OwnerCheckBNB
+      if (ChainSelect === 0) {
+        MsgArea.classList.add('Error')
+        MsgArea.innerText = `Unsupported network detected!\nPlease set Metamask Network to Harmony Shard 0 Mainnet, Binance Smart Chain Mainnet or Celo Mainnet.`
+        TitleText.innerText = 'Please change to the supported Metamask network and then refresh the page.'
+      } else {
+        ownNFTbalance = await CheckOwner (ChainName[ChainSelect], ChainShort[ChainSelect], ChainSym[ChainSelect], ChainConABI[ChainSelect], ChainPrice[ChainSelect])
       }
 
-      if (PassOwner > 0) {
+      if (ownNFTbalance > 0) {
         console.log('Allow display of restricted posts.')
         TitleText.innerText = 'Welcome Back!\nYou may still be able to purchase more passes.'
-        AREAm.innerText = 'Thank you for your support! You are truely an amazing supporter!'
-        ContentArea.innerHTML += '<object style="display: flex; width: 100%; height: 600px;" type="text/html" data="https://asvoria.github.io/Author/bundled/display.html"></object>'
+        MsgArea.innerText = 'Thank you for your support! You are truely an amazing supporter!'
+        CntArea.innerHTML += '<object style="display: flex; width: 98%; height: 560px;" type="text/html" data="https://asvoria.github.io/Author/bundled/display.html"></object>'
+      } else {
+        console.log('Allow display of restricted posts.')
+        TitleText.innerText = 'Welcome to Shomin\'s Secret Corner!\nClick the following buttons to buy the Secret Corner Pass:'
       }
-
-      BUTTONb.classList.add('hideclass')
-      BUTTONb.classList.remove('is-visible')
       TitleText.classList.add('is-visible')
       TitleText.classList.remove('hideclass')
-      TitleText.innerText = 'Welcome to Shomin\'s Secret Corner!\nClick the following buttons to buy the Secret Corner Pass on their dedicated network:'
-
     } catch (error) {
       console.error('error')
       console.error(error)
     }
 
-    BUTTON.onclick = onClickConnect
-    buyBUTTONbONE.onclick = onClickBuyONE
-    buyBUTTONbBNB.onclick = onClickBuyBNB
+    BTNlogin.onclick = onClickConnect
+    buyBTN.onclick = onClickBuyX (ChainName[ChainSelect], ChainID[ChainSelect], ChainWeb3[ChainSelect], ChainConABI[ChainSelect], ChainPrice[ChainSelect], ChainStrURL[ChainSelect], ChainConAdr[ChainSelect])
   }
 
   const onClickInstall = () => {
-    BUTTON.innerText = 'Onboarding in progress'
+    BTNlogin.innerText = 'Onboarding in progress'
     onboarding.startOnboarding()
   }
 
   const MetaMaskClientCheck = () => {
-    buyBUTTONbONE.classList.add('is-visible')
-    buyBUTTONbONE.classList.remove('hideclass')
-    buyBUTTONbBNB.classList.add('is-visible')
-    buyBUTTONbBNB.classList.remove('hideclass')
-    BUTTONb.classList.add('is-visible')
-    BUTTONb.classList.remove('hideclass')
+    buyBTN.classList.remove('is-visible')
+    buyBTN.classList.add('hideclass')
+    BTNlogin.classList.add('is-visible')
+    BTNlogin.classList.remove('hideclass')
     if (isMetaMaskInstalled()) {
-      BUTTON.innerText = 'LOGIN with METAMASK!'
-      BUTTON.onclick = onClickConnect
+      BTNlogin.innerText = 'LOGIN with METAMASK!'
+      BTNlogin.onclick = onClickConnect
     } else {
-      BUTTON.innerText = 'INSTALL METAMASK!'
-      BUTTON.onclick = onClickInstall
+      BTNlogin.innerText = 'INSTALL METAMASK!'
+      BTNlogin.onclick = onClickInstall
     }
   }
   MetaMaskClientCheck()
-  BUTTON.onclick = onClickConnect
-  buyBUTTONbONE.onclick = onClickBuyONE
-  buyBUTTONbBNB.onclick = onClickBuyBNB
+  BTNlogin.onclick = onClickConnect
 }
 
 runMetamask()
@@ -323,11 +237,9 @@ runMetamask()
 window.addEventListener('DOMContentLoaded', () => {
   TitleText.classList.add('hideclass')
   runMetamask()
-  buyBUTTONbONE.classList.add('hideclass')
-  buyBUTTONbONE.classList.remove('is-visible')
-  buyBUTTONbBNB.classList.add('hideclass')
-  buyBUTTONbBNB.classList.remove('is-visible')
-  BUTTONb.classList.add('is-visible')
-  BUTTONb.classList.remove('hideclass')
+  buyBTN.classList.add('hideclass')
+  buyBTN.classList.remove('is-visible')
+  BTNlogin.classList.add('is-visible')
+  BTNlogin.classList.remove('hideclass')
   console.log('DOM fully loaded and parsed')
 })
