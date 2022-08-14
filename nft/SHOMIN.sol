d217121b6d008aede538a1e22502244c16dd4ea5
ThriftyOldStudent <asvoria@live.com>
2022-08-14 12:02:33 +0800

    y

--- nft/SHOMIN.sol
+++ nft/SHOMIN.sol
@@ -2,9 +2,22 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract SSC is ERC721URIStorage {
+library Counters {
+    struct Counter {
+        uint256 _value; // default: 0
+    }
+    function current(Counter storage counter) internal view returns (uint256) {
+        return counter._value;
+    }
+    function increment(Counter storage counter) internal {
+        unchecked {
+            counter._value += 1;
+        }
+    }
+}
+
+contract SSCCL is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     address payable private deployWallet;
